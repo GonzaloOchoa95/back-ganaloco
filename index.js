fc64fb47f775178ecb277da9d1c57e31bd99e04c
GonzaloOchoa95 <gonzalo.afanador01@unicatolica.edu.co>
2024-10-26 02:00:19 -0500

    update

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const connectDB = require('./db/mongo');
-
-//const mongoose = require('mongoose')
-//const bcrypt = require('bcrypt')
-//const jwt = require('jsonwebtoken')
-//const expressjwt = require('express-jwt')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const expressjwt = require('express-jwt')
 const {urlencoded, json} = require('express');
 const router = require('./routes/routes.js');
 const cors = require('cors');
 
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT;
 connectDB();
-
-//mongoose.connect('mongodb+srv://adm:Cali23809667*@bd-ganaloco.a9awg.mongodb.net/bd-ganaloco?retryWrites=true&w=majority&appName=bd-ganaloco',{})
 
 const app = express();
 app.use(express.json())
@@ -21,17 +17,11 @@ app.use(express.json())
 
 app.use(urlencoded({extended: true}))
 
-//app.use(cors())
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+app.use(cors())
 app.use('/v1/signos', router);
 
 
 
 app.listen(port, ()=>{
-    console.log(`Listening at port ${port}`);
-
+    console.log('listening at port 4000');
 })

8c8eaad0e9655eee63ddd828e7fdcf3e2dddc535
GonzaloOchoa95 <gonzalo.afanador01@unicatolica.edu.co>
2024-10-26 01:11:45 -0500

    update index

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 const express = require('express');
-const connectDB = require('./db/mongo');
-const mongoose = require('mongoose')
-//const bcrypt = require('bcrypt')
-//const jwt = require('jsonwebtoken')
-//const expressjwt = require('express-jwt')
-const {urlencoded, json} = require('express');
+const mongoose = require('mongoose');
+const { urlencoded, json } = require('express');
 const router = require('./routes/routes.js');
 const cors = require('cors');
 
-//const port = process.env.PORT;
-//connectDB();
+const port = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect('mongodb+srv://adm:Cali23809667*@bd-ganaloco.a9awg.mongodb.net/bd-ganaloco?retryWrites=true&w=majority&appName=bd-ganaloco',{})
+if (!MONGODB_URI) {
+  console.error('MONGODB_URI no está definido en las variables de entorno');
+  process.exit(1);
+}
+
+mongoose.connect(MONGODB_URI, {})
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch(err => {
+    console.error('Error conectando a MongoDB:', err);
+    process.exit(1);
+  });
 
 const app = express();
-app.use(express.json())
 
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
-app.use(urlencoded({extended: true}))
-
-//app.use(cors())
 app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use('/v1/signos', router);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal!');
+});
 
-
-app.listen(port, ()=>{
-    console.log('listening at port 5000');
-})
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+}).on('error', (err) => {
+  console.error('Error al iniciar el servidor:', err);
+  process.exit(1);
+});

aec5da2291867f51871a47549a2f8bc48ee68138
GonzaloOchoa95 <gonzalo.afanador01@unicatolica.edu.co>
2024-10-26 01:51:48 -0500

    update

--- routes/controllers/validarController.js
+++ routes/controllers/validarController.js
@@ -5,7 +5,7 @@ const modeCodigo = require('../../db/codigo')
 const regisCodigo = require('../../db/registroCodigo')
 const moment = require('moment-timezone');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const mongodb = require('../../db/mongo')
 

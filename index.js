7bd90b3b4ad4d75e17632e37cb5f157d09c1a164
GonzaloOchoa95 <gonzalo.afanador01@unicatolica.edu.co>
2024-10-26 01:20:39 -0500

    update index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./db/mongo');
-const mongoose = require('mongoose')
+//const mongoose = require('mongoose')
 //const bcrypt = require('bcrypt')
 //const jwt = require('jsonwebtoken')
 //const expressjwt = require('express-jwt')
@@ -11,7 +11,7 @@ const cors = require('cors');
 const port = process.env.PORT;
 connectDB();
 
-mongoose.connect('mongodb+srv://adm:Cali23809667*@bd-ganaloco.a9awg.mongodb.net/bd-ganaloco?retryWrites=true&w=majority&appName=bd-ganaloco',{})
+//mongoose.connect('mongodb+srv://adm:Cali23809667*@bd-ganaloco.a9awg.mongodb.net/bd-ganaloco?retryWrites=true&w=majority&appName=bd-ganaloco',{})
 
 const app = express();
 app.use(express.json())

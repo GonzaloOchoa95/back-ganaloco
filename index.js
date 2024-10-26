df2a7bced4cfc6835202e3ff076f0fa91f7932ad
GonzaloOchoa95 <gonzalo.afanador01@unicatolica.edu.co>
2024-10-26 00:42:22 -0500

    Update back

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const {urlencoded, json} = require('express');
 const router = require('./routes/routes.js');
 const cors = require('cors');
 
-const port = process.env.PORT;
-connectDB();
+//const port = process.env.PORT;
+//connectDB();
+
+mongoose.connect('mongodb+srv://adm:Cali23809667*@bd-ganaloco.a9awg.mongodb.net/bd-ganaloco?retryWrites=true&w=majority&appName=bd-ganaloco',{})
 
 const app = express();
 app.use(express.json())
@@ -17,7 +19,12 @@ app.use(express.json())
 
 app.use(urlencoded({extended: true}))
 
-app.use(cors())
+//app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 app.use('/v1/signos', router);
 
 
